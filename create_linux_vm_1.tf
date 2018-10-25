ed53c2ccc24a4149c4d9cfc6d7aee659b4e1107f
sjohner <sjohner@posteo.ch>
2018-10-25 12:14:38 +0200

    Change resource names

--- create_linux_vm_1.tf
+++ create_linux_vm_1.tf
@@ -8,7 +8,7 @@ resource "azurerm_resource_group" "resourcegroup" {
 }
 
 resource "azurerm_virtual_network" "virtualnetwork" {
-    name                = "${azurerm_virtual_machine.virtualmachine.name}VNET"
+    name                = "azl${random_id.randomId.hex}VNET"
     address_space       = ["10.0.0.0/16"]
     location            = "westeurope"
     resource_group_name = "${azurerm_resource_group.resourcegroup.name}"
@@ -19,14 +19,14 @@ resource "azurerm_virtual_network" "virtualnetwork" {
 }
 
 resource "azurerm_subnet" "subnet" {
-    name                 = "${azurerm_virtual_machine.virtualmachine.name}Subnet"
+    name                 = "azl${random_id.randomId.hex}Subnet"
     resource_group_name  = "${azurerm_resource_group.resourcegroup.name}"
     virtual_network_name = "${azurerm_virtual_network.virtualnetwork.name}"
     address_prefix       = "10.0.2.0/24"
 }
 
 resource "azurerm_public_ip" "publicip" {
-    name                         = "${azurerm_virtual_machine.virtualmachine.name}PublicIP"
+    name                         = "azl${random_id.randomId.hex}PublicIP"
     location                     = "westeurope"
     resource_group_name          = "${azurerm_resource_group.resourcegroup.name}"
     public_ip_address_allocation = "dynamic"
@@ -37,7 +37,7 @@ resource "azurerm_public_ip" "publicip" {
 }
 
 resource "azurerm_network_security_group" "networksecuritygroup" {
-    name                = "${azurerm_virtual_machine.virtualmachine.name}NSG"
+    name                = "azl${random_id.randomId.hex}NSG"
     location            = "westeurope"
     resource_group_name = "${azurerm_resource_group.resourcegroup.name}"
 
