de95c6fbe698b9666501365571eac42d972ce8cd
sjohner <sjohner@posteo.ch>
2018-11-13 11:02:04 +0100

    Removed keeper from randomId resource

--- create_linux_vm.tf
+++ create_linux_vm.tf
@@ -85,11 +85,6 @@ resource "azurerm_network_interface" "nic" {
 }
 
 resource "random_id" "randomId" {
-#    keepers = {
-        # Generate a new ID only when a new resource group is defined
-#        resource_group = "${azurerm_resource_group.resourcegroup.name}"
-#    }
-
     byte_length = 8
 }
 
