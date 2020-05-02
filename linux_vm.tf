7c2064273527f0e6d84120192316b7a2519eaff6
Stefan Johner <sjohner@Donald.home>
2020-05-02 16:02:54 +0200

    Added cloud init script and various outputs

--- linux_vm.tf
+++ linux_vm.tf
@@ -1,3 +1,10 @@
+data "azurerm_platform_image" "ubuntu_server" {
+  location  = var.location
+  publisher = "Canonical"
+  offer     = "UbuntuServer"
+  sku       = var.image_sku
+}
+
 resource "random_id" "randomId" {
     byte_length = 4
 }
@@ -70,7 +77,7 @@ resource "azurerm_network_interface" "nic" {
     }
 }
 
-resource "azurerm_network_interface_security_group_association" "example" {
+resource "azurerm_network_interface_security_group_association" "default_nsg_association" {
   network_interface_id = azurerm_network_interface.nic.id
   network_security_group_id = azurerm_network_security_group.networksecuritygroup.id
 }
@@ -92,7 +99,11 @@ resource "azurerm_linux_virtual_machine" "virtualmachine" {
     admin_username = var.admin_username
     disable_password_authentication = true
     computer_name  = "azl${random_id.randomId.hex}"
+    source_image_id = data.azurerm_platform_image.ubuntu_server.id
     tags = var.tags
+
+    custom_data = base64encode(data.template_file.linux-vm-cloud-init.rendered)
+
     network_interface_ids = [
         azurerm_network_interface.nic.id
     ]
@@ -108,14 +119,27 @@ resource "azurerm_linux_virtual_machine" "virtualmachine" {
         storage_account_type = "StandardSSD_LRS"
     }
 
-    source_image_reference {
-        publisher = "Canonical"
-        offer = "UbuntuServer"
-        sku = "18.04-LTS"
-        version = "latest"
-    }
-
     boot_diagnostics {
         storage_account_uri = azurerm_storage_account.storageaccount.primary_blob_endpoint
     }
 }
+
+# Get public ip address from newly created virtual machine
+data "azurerm_public_ip" "example" {
+    name = azurerm_public_ip.publicip.name
+    resource_group_name = azurerm_linux_virtual_machine.virtualmachine.resource_group_name
+}
+
+# Read bash cloud init file
+data "template_file" "linux-vm-cloud-init" {
+  template = file("cloud_init.sh")
+}
+
+output "public_ip_address" {
+  value = data.azurerm_public_ip.example.ip_address
+}
+
+output "vm_image_version" {
+  value = data.azurerm_platform_image.ubuntu_server.version
+}
+
--- variables.tf
+++ variables.tf
@@ -1,19 +1,44 @@
 provider "azurerm" {
+    # Whilst version is optional, it is strongly recommend using it to pin the version of the Provider being used
     version = "=2.7.0"
+
     subscription_id = var.subscription_id
+    client_id       = var.client_id
+    client_secret   = var.client_secret
+    tenant_id       = var.tenant_id
+    
     features {}
 }
 
 variable "subscription_id" {
-    description = "Subscription ID for provisioning resources in Azure"
+    description = "Id of the subscription in which resources are deployed"
+}
+
+variable "client_secret" {
+    description = "Secret of the service principal used to authenticate to Azure"
+}
+
+variable "client_id" {
+    description = "Service Principal Id to authenticate to Azure"
+}
+
+variable "tenant_id" {
+    description = "Id of the AAD tenant you are authenticating against"
 }
 
 variable "location" {
-    description = "The default Azure region for the resource provisioning"
+    default = "switzerlandnorth"
+    description = "The Azure region where resources are deployed"
 }
 
 variable "vm_size" {
-    description = "The default virtual machine SKU"
+    default = "Standard_D1_v2"
+    description = "The virtual machine size"
+}
+
+variable "vm_image_sku" {
+    default = "18.04-LTS"
+    description = "The virtual machine image SKU to be used"
 }
 
 variable "admin_username" {
