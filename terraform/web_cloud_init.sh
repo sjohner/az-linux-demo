2e444671a0db46cfc6f06a4dd25a75419b7aadc1
Stefan Johner <16019080+sjohner@users.noreply.github.com>
2020-11-26 15:33:15 +0100

    Update web_cloud_init.sh

--- terraform/web_cloud_init.sh
+++ terraform/web_cloud_init.sh
@@ -3,7 +3,7 @@ apt-get update
 apt-get install -y apache2 docker.io
 git clone https://github.com/sjohner/jhnr.ch.git src
 git clone https://github.com/sjohner/hyde.git src/themes/hyde
-docker run --rm --name "hugo" -v $(pwd)/src:/src -v $(pwd)/output:/output -e HUGO_BASEURL="http://jhnr-blog-prod.westeurope.cloudapp.azure.com/" -e HUGO_THEME="hyde" jojomi/hugo
+docker run --rm --name "hugo" -v $(pwd)/src:/src -v $(pwd)/output:/output -e HUGO_TITLE="Powercoders" -e HUGO_BASEURL="http://jhnr-blog-prod.westeurope.cloudapp.azure.com/" -e HUGO_THEME="hyde" jojomi/hugo
 mv $(pwd)/output/* /var/www/html/
 systemctl start apache2
 systemctl enable apache2
